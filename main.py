b5ef814dd2faa1fdf2b5b70ddd38c8d553dfecd0
dust007 <fanfxj@gmail.com>
2017-10-27 16:37:43 -0700

    v2

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ print("y shape ", y_train.shape)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
@@ -43,6 +43,8 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train,
+          batch_size=64, epochs=20,
+          validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
