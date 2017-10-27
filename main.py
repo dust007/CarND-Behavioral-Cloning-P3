071e1e5b6b6cea553211287abd3ccf3b34a9c31f
Maxwell <ffvoyaged@gmail.com>
2017-10-26 22:14:46 -0700

    v1

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import csv
 import cv2
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
+from keras.layers import Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 import numpy as np
 
 lines = []
-with open('./data/driving_log1.csv') as csvfile:
+with open('./data/driving_log2.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -16,6 +19,7 @@ for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = './data/IMG/' + filename
+    # print(current_path)
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -24,8 +28,18 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measures)
 
+print("X shape ", X_train.shape)
+print("y shape ", y_train.shape)
+
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
