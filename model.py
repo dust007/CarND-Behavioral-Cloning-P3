a8dc8cb8531e3e18d9d947667ce5e32fc76e9173
Maxwell <ffvoyaged@gmail.com>
2017-11-02 10:30:17 -0700

    v8

--- model.py
+++ model.py
@@ -1,19 +1,20 @@
 import csv
 import cv2
+from PIL import Image
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
-from keras.layers import Lambda
-from keras.layers import Cropping2D
+from keras.layers import Flatten, Dense, Dropout, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pyplot
 
 images = []
 measures = []
 
 # load data
-with open('./data/driving_log2.csv') as csvfile:
+with open('./data/driving_log_all.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         row = []
@@ -30,9 +31,9 @@ with open('./data/driving_log2.csv') as csvfile:
         steering_right = steering_center - correction
 
         # read in images from center, left and right cameras
-        img_center = cv2.imread(path + row[0])
-        img_left = cv2.imread(path + row[1])
-        img_right = cv2.imread(path + row[2])
+        img_center = np.asarray(Image.open(path + row[0]))
+        img_left = np.asarray(Image.open(path + row[1]))
+        img_right = np.asarray(Image.open(path + row[2]))
 
         # add images and angles to data set
         images.extend([img_center, img_left, img_right])
@@ -60,20 +61,31 @@ model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Dropout(0.5))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
+model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-# early stop to prevent overfitting
-early_stop = EarlyStopping(monitor='val_loss', patience=3, verbose=1)
-model.fit(X_train, y_train,
-          nb_epoch=4, batch_size=64,
+# checkpoint
+filepath="weights-{epoch:02d}-{val_loss:.4f}.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
+history = model.fit(X_train, y_train,
+          nb_epoch=20, batch_size=128,
           validation_split=0.2, shuffle=True,
-          callbacks=[early_stop], verbose=1)
-# save model
-model.save('model.h5')
+          callbacks=callbacks_list, verbose=1)
+
+# save train loss and valid loss plot
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.savefig('./loss_hist.png')
